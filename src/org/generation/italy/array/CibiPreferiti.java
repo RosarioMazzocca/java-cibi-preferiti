0a615e9200ecbec29472c690f11e0634db223b10
RosarioMazzocca <mazzocca.rosario@libero.it>
2021-11-17 15:24:19 +0100

    Aggiunto calcolo della posizione

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -5,10 +5,11 @@ public class CibiPreferiti {
 		
 		String[] cibiPreferiti = {"Patate", "Tiramisù", "Pizza", "Sushi", "Ramen", "Cotoletta"};
 		System.out.println("Numero di elementi della classifica: " + cibiPreferiti.length);
+		int lunghezzaArray = cibiPreferiti.length;
 		
 		System.out.println("Il mio cibo preferito in assoluto: " + cibiPreferiti[0]);
-		System.out.println("il mio cibo preferito ma non troppo: " + cibiPreferiti[5]);
-		System.out.println("il mio cibo preferito nè in assoluto nè che disdegno: " + cibiPreferiti[3]);
+		System.out.println("il mio cibo preferito ma non troppo: " + cibiPreferiti[lunghezzaArray-1]);
+		System.out.println("il mio cibo preferito nè in assoluto nè che disdegno: " + cibiPreferiti[lunghezzaArray/2]);
 			
 	}
 	
